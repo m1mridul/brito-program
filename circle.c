83999d1c5b0b9139813d2f626796ac5930864df0
m1mridul <mridulhasan870@gmail.com>
2023-03-24 16:23:38 +0600

    first commit

---
+++ circle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    double R,A;
+    const double P = 3.1416;
+    printf("enter the value of R:");
+    scanf("lf",&R);
+    A=P*R*R;
+    printf("A=%lf*%lf*%lf",P,R,R,A);
+
+    return 0;
+}
